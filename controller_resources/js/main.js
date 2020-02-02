67c20d2762dc53059c777624fd9d7f93f41a6768
maaryhabad <maryh.abad@gmail.com>
2020-02-02 02:51:13 -0300

    Primeiro commit com airconsole funcionando

---
+++ controller_resources/js/main.js
@@ -0,0 +1,77 @@
+/* main.js */
+
+var airConsole = null;
+var downEvent;
+var upEvent;
+
+
+
+$(document).ready(function() {
+
+	airConsole = new AirConsole({"orientation": "landscape"});
+	
+	downEvent = isMobile() ? 'touchstart' : 'mousedown';
+	upEvent = isMobile() ? 'touchend' : 'mouseup';
+
+
+	var sendHandshake = function() {
+		airConsole.message(AirConsole.SCREEN, {
+			handshake: true
+		});
+	};
+
+	airConsole.onReady = function() {
+		sendHandshake();
+	}
+
+
+	var sendMessage = function(string) {
+		airConsole.message(AirConsole.SCREEN, {
+			message: string
+		});
+	};
+
+	airConsole.onMessage = function(deviceId, data) {
+		if (data.handshake) {
+			sendHandshake();
+		}
+	};
+	
+	$('#up').on(downEvent, function (event) {
+		sendMessage('up');
+	});
+	
+	$('#up').on(upEvent, function (event) {
+		sendMessage('stop');
+	});
+	
+	$('#down').on(downEvent, function (event) {
+		sendMessage('down');
+	});
+	
+	$('#down').on(upEvent, function (event) {
+		sendMessage('stop');
+	});
+});
+
+/**
+* Returns true if device is a mobile device
+* @return {Boolean}
+*/
+function isMobile() {
+	if (navigator.userAgent.match(/Android/i) ||
+		navigator.userAgent.match(/webOS/i) ||
+		navigator.userAgent.match(/iPhone/i) ||
+		navigator.userAgent.match(/iPad/i) ||
+		navigator.userAgent.match(/iPod/i) ||
+		navigator.userAgent.match(/BlackBerry/i) ||
+		navigator.userAgent.match(/Windows Phone/i) ||
+		navigator.userAgent.match(/WPDesktop/i) ||
+		navigator.userAgent.match(/Opera Mini/i) ||
+		navigator.userAgent.match(/IEMobile/i)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
